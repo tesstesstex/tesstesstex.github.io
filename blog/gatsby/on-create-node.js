99a224b776ef4dff27d7227ff662c52056e5ed1c
Tessho Nakamura <tesstesstex@gmail.com>
2021-06-10 23:20:42 +0900

    BUILD TEST

--- blog/gatsby/on-create-node.js
+++ blog/gatsby/on-create-node.js
@@ -13,31 +13,29 @@ const onCreateNode = ({ node, actions, getNode }) => {
     // node.internal.type === 'SitePage'
     // typeof node.context.slug !== 'undefined'
   ) {
-    if (typeof node.context !== 'undefined') {
-      if (typeof node.context.slug !== 'undefined') {
-        createNodeField({
-          node,
-          name: 'slug',
-          value: `/${node.context.slug}`,
-        });
-      } else {
-        const value = createFilePath({ node, getNode });
-        createNodeField({
-          node,
-          name: 'slug',
-          value,
-        });
-      }
+    if (_.get(node.context.slug) !== 'undefined') {
+      createNodeField({
+        node,
+        name: 'slug',
+        value: `/${node.context.slug}`,
+      });
+    } else {
+      const value = createFilePath({ node, getNode });
+      createNodeField({
+        node,
+        name: 'slug',
+        value,
+      });
+    }
 
-      if (node.tags) {
-        const tagSlugs = node.tags.map(tag => `/tag/${_.kebabCase(tag)}/`);
-        createNodeField({ node, name: 'tagSlugs', value: tagSlugs });
-      }
+    if (node.tags) {
+      const tagSlugs = node.tags.map(tag => `/tag/${_.kebabCase(tag)}/`);
+      createNodeField({ node, name: 'tagSlugs', value: tagSlugs });
+    }
 
-      if (node.category) {
-        const categorySlug = `/category/${_.kebabCase(node.category)}/`;
-        createNodeField({ node, name: 'categorySlug', value: categorySlug });
-      }
+    if (node.category) {
+      const categorySlug = `/category/${_.kebabCase(node.category)}/`;
+      createNodeField({ node, name: 'categorySlug', value: categorySlug });
     }
   }
 };
