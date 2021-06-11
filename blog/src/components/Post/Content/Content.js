e74ef6a34aa49462cb58d2b70d7ffb1e1f589068
Tessho Nakamura <tesstesstex@gmail.com>
2021-06-11 22:26:53 +0900

    Used markdown for post's body

--- blog/src/components/Post/Content/Content.js
+++ blog/src/components/Post/Content/Content.js
@@ -1,16 +1,30 @@
 // @flow strict
 import React from 'react';
 import styles from './Content.module.scss';
+import sanitize from 'sanitize-html';
+import marked from 'marked';
 
 type Props = {
   body: string,
-  title: string
+  title: string,
+};
+
+const markdown = data => {
+  console.log(data)
+  console.log(marked(data))
+  console.log(sanitize(marked(data), { allowedTags: sanitize.defaults.allowedTags.concat(['img']) }))
+  return sanitize(marked(data), {
+    allowedTags: sanitize.defaults.allowedTags.concat(['img']),
+  });
 };
 
 const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+    <div
+      className={styles['content__body']}
+      dangerouslySetInnerHTML={{ __html: markdown(body) }}
+    />
   </div>
 );
 
